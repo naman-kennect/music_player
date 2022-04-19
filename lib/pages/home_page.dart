6c444da943816f415a0a645435fac253c89117d8
naman-kennect <naman.kennect@gmail.com>
2022-04-19 21:58:01 +0530

    ,

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:music_player/model/radio.dart';
@@ -11,10 +13,25 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<MyRadio> radios;
+  MyRadio _selectedRadio;
+  Color _selectedColor;
+  bool _isPlaying = false;
+
+  final AudioPlayer _audioPlayer = AudioPlayer();
+
   @override
   void initState() {
     super.initState();
     fetchRadios();
+
+    _audioPlayer.onPlayerStateChanged.listen((event) {
+      if (event == AudioPlayerState.PLAYING) {
+        _isPlaying = true;
+      } else {
+        _isPlaying = false;
+      }
+      setState(() {});
+    });
   }
 
   fetchRadios() async {
@@ -25,6 +42,14 @@ class _HomePageState extends State<HomePage> {
     setState(() {});
   }
 
+  _playMusic(String url) {
+    _audioPlayer.play(url);
+    _selectedRadio = radios.firstWhere((element) => element.url == url);
+    print(_selectedRadio.name);
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: Drawer(),
@@ -45,7 +70,7 @@ class _HomePageState extends State<HomePage> {
               .make(),
           AppBar(
             title: "VibeZy".text.xl4.bold.white.make().shimmer(
-                  primaryColor: Vx.cyan100,
+                  primaryColor: Vx.blue300,
                   secondaryColor: Colors.white,
                 ),
             backgroundColor: Colors.transparent,
@@ -59,8 +84,46 @@ class _HomePageState extends State<HomePage> {
             itemBuilder: (context, index) {
               final rad = radios[index];
               return VxBox(
-                child: ZStack([]),
+                child: ZStack(
+                  [
+                    Positioned(
+                      top: 0.0,
+                      right: 0.0,
+                      child: VxBox(
+                              child: rad.category.text.uppercase.white
+                                  .make()
+                                  .px16())
+                          .height(40)
+                          .black
+                          .alignCenter
+                          .withRounded(value: 10.0)
+                          .make(),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: VStack(
+                        [
+                          rad.name.text.xl3.white.bold.make(),
+                          5.heightBox,
+                          rad.tagline.text.sm.white.semiBold.make(),
+                        ],
+                        crossAlignment: CrossAxisAlignment.center,
+                      ),
+                    ),
+                    Align(
+                        alignment: Alignment.center,
+                        child: [
+                          Icon(
+                            CupertinoIcons.play_circle,
+                            color: Colors.white,
+                          ),
+                          10.heightBox,
+                          "tap to play".text.gray300.make(),
+                        ].vStack()),
+                  ],
+                ),
               )
+                  .clip(Clip.antiAlias)
                   .bgImage(
                     DecorationImage(
                       image: NetworkImage(rad.image),
@@ -69,17 +132,40 @@ class _HomePageState extends State<HomePage> {
                         Colors.black.withOpacity(0.3),
                         BlendMode.darken,
                       ),
-                      
                     ),
-                  ).withRounded(value: 60.0)
-                  .border(color: Colors.black,width: 5.0)
+                  )
+                  .withRounded(value: 60.0)
+                  .border(color: Colors.black, width: 3.0)
                   .make()
-                  .p16()
-                  .centered();
+                  .onInkTap(() {
+                    _playMusic(rad.url);
+                  })
+                  .p16();
             },
-          )
+          ).centered(),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: [
+              if (_isPlaying)
+                "Playing now - ${_selectedRadio.name}".text.makeCentered(),
+              Icon(
+                _isPlaying
+                    ? CupertinoIcons.stop_circle
+                    : CupertinoIcons.play_circle,
+                color: Colors.white,
+                size: 50.0,
+              ).onInkTap(() {
+                if(_isPlaying){
+                  _audioPlayer.stop();
+                } else{
+                  _playMusic(_selectedRadio.url);
+                }
+              })
+            ].vStack(),
+          ).pOnly(bottom: context.percentHeight * 12)
         ],
         fit: StackFit.expand,
+        clipBehavior: Clip.antiAlias,
       ),
     );
   }
